4a3f1f72be556678f0687b960538502b915b14d4
vinitkadam <vinitkadam1997@gmail.com>
2017-02-16 21:21:55 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?php
-$servername = 'localhost';
-$username = 'root';
-$password = '';
+$servername = "localhost";
+$username = "root";
+$password = "";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
