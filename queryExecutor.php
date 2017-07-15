37bf0a86d8f4a9880698dad81edde998babc0420
vinitkadam <vinitkadam1997@gmail.com>
2017-07-15 20:58:00 +0530

    Add files via upload

--- queryExecutor.php
+++ queryExecutor.php
@@ -99,7 +99,56 @@ case "TruncateTable":
 		echo "Database does not exist";
 	}
 	break;
+	
+case "desc":
+	$DB_Name=$_POST['dbName'];
+	$query_Stat=$_POST['Query'];
+	$tablename=$_POST['tName'];
+	if($conn->select_db($DB_Name))
+	{
+		$result=mysqli_query($conn,$query_Stat);
+		/*echo "<br><b>".$tablename." structure</b><br><br>";*/
+		echo "
+		<tr>
+		<td align=center> <b>FIELD</b></td>
+		<td align=center><b>TYPE</b></td>
+		<td align=center><b>NULL</b></td>
+		<td align=center><b>KEY</b></td></td>
+		<td align=center><b>DEFAULT</b></td>
+		<td align=center><b>EXTRA</b></td>";
 
+		while($data = mysqli_fetch_row($result))
+		{   
+			echo "<tr>";
+			echo "<td align=center>$data[0]</td>";
+			echo "<td align=center>$data[1]</td>";
+			echo "<td align=center>$data[2]</td>";
+			echo "<td align=center>$data[3]</td>";
+			echo "<td align=center>$data[4]</td>";
+			echo "<td align=center>$data[5]</td>";
+			echo "</tr>";
+		}
+		
+	}
+	else {
+		echo "Database does not exist";
+	}
+case "addColumn":
+	$DB_Name=$_POST['dbName'];
+	$query_Stat=$_POST['Query'];
+	if($conn->select_db($DB_Name))
+	{
+		if($conn->query($query_Stat)=== TRUE){
+			echo "columns addded succesfully";
+		}
+		else{
+			echo "error occured while adding tables. Try again.."; 
+		}
+	}
+	else {
+		echo "Database does not exist";
+	}
+	break;
 default:
     echo "Nothing to show";
 }
